a1a1eadf2f16547656b8cfaa9683d8480119ea16
mghanstad <mghanstad@gmail.com>
2018-07-13 15:45:19 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -7,10 +7,12 @@ class River
     @name = name
   end
   def flood
+    dischage = :discharge
     discharge * 1.3
   end 
   
   def dry_up
+    discharge = :discharge
     discharge * 0.5
   end  
 end
