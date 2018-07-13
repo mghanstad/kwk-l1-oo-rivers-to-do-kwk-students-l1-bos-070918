84be36f7b62d907506ad8932a044885802d3cb35
mghanstad <mghanstad@gmail.com>
2018-07-13 15:37:39 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,21 @@
 # river.rb
+class River
+  attr_writer :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  def flood
+    discharge*1.3
+  end 
+  
+  def dry_up
+    discharge*.5
+  end  
+end
+  
+nile = River.new("The Nile")
+mississippi = River.new("The Mississippi")
+amazon = River.new("The Amazon")
+seine = River.new("The Seine")
+yangtze = River.new("The Yangtze")
+euphrates = River.new("The Euphrates")
